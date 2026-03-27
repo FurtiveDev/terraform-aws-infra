56c787ff38952ac8b96e30993f405a19a84c3ded
FurtiveDev <FurtiveDev@users.noreply.github.com>
2026-03-28 00:03:51 +0300

    add S3 backend for state management

--- main.tf
+++ main.tf
@@ -7,6 +7,12 @@ terraform {
       version = "~> 5.0"
     }
   }
+
+  backend "s3" {
+    bucket = "terraform-state-webapp"
+    key    = "terraform.tfstate"
+    region = "eu-west-1"
+  }
 }
 
 provider "aws" {
